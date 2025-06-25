23b50d4ae45e91622bf0ce4f2f1ea23ee6cda0f3
Vineet Chilwal <vineetschilwal@gmail.com>
2025-06-25 11:52:43 +0530

    add function to retrieve current user's watch history with video and owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -435,6 +435,62 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+// Function to get watch history
+// This function retrieves the watch history of the current user, including video details and owner information.
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    );
+});
+
 // Exporting the functions to be used in routes
 export {
   registerUser,
@@ -446,4 +502,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
